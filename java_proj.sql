9b3f98d7bc4a2c63dba6503be246ea266527bfbd
RadhikaPisipati <145963959+RadhikaPisipati@users.noreply.github.com>
2024-04-25 12:31:01 +0530

    Add files via upload

---
+++ java_proj.sql
@@ -0,0 +1,4 @@
+create database portalSystem;
+use portalSystem;
+create table signup(form_no varchar (30), name varchar(30), father_name varchar(30), DOB varchar(30), gender varchar(30), email varchar(60), department varchar(30), address varchar(60), city varchar(30), pincode varchar(30), state varchar(50));
+select * from signup;
